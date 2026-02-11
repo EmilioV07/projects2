5b16176fdb182d494766ecb9367ce0169b8eee4b
EmilioV07 <493914@bsd48.org>
2026-02-11 13:04:37 -0800

    LT2/11

--- ht/main.cpp
+++ ht/main.cpp
@@ -3,90 +3,93 @@
 #include<vector>
 #include<random>
 #include<fstream>
-#include"firstnames.txt"
-#include"lastnames.txt"
 using namespace std;
-struct student{string name;int id;double gpa;, student* next, student* prev}//ADD DESTRUCTOR
-void print(student** &table){//prints all students
+struct student{
+	string name;
+	int id;
+	double gpa;
+	student* next;
+	student();
+	student(string n, int i, double g, student* nx=nullptr)
+		: name(n), id(i), gpa(g), next(nx), {}
+	};
+void print(student** &table, int &tablesize){//prints all students
 	//iterate through headlist
 		//iterate through chains and print items
 }
-void dl(student** &table){//deletes one student
+void dl(student** &table, int &tablesize){//deletes one student
 	int _target;
+	string _targetstr;
 	cout<<"You are deleting a student"<<endl;
-	cout<<"ID of student-to-delete: "
-	getline(cin, _target);
+	cout<<"ID of student-to-delete: ";
+	getline(cin, _targetstr);
+	_target=stoi(_targetstr);
 	cout<<endl;
 	//DELETE THE TARGET
 }
-bool rehash(student** &table){//automatically (when called) re-spreads the items into a new hash table
-	bool rhtf;
-	
-	student** newtable = new student*[2*(sizeof(table)/sizeof(table[0]))];//assigns 'global' table variable to new table of double size
-	for(int i=0;i<(sizeof(newtable)/sizeof(newtable[0]));i++){student* h=nullptr;newtable[i]=h;}//fills table with empty head pointers
-	
-	for(student* item ; table){//FOR EVERY OLD STUDENT
-		if(item==nullptr){continue;}//IF THERE IS NO STUDENT
-		if(item!=nullptr){//IF THERE IS A STUDENT
-			int index;
-			int chainlen = 1;
-			bool placing;
-			student* oldtemp = item;
-			student* newtemp = newtable[index];
-			for(char i ; oldtemp->_name){index += (static_cast<int>(i) + 47) * 109;
-			index %= (sizeof(newtable)/sizeof(newtable[0]))
-			while(placing)
-				if(newtemp==nullptr){//IF SPOT UNOCCUPIED
-					newtemp=oldtemp;//place
-					oldtemp=oldtemp->next;//iterate
-					newtemp->next=nullptr;//patch
-				}
-				else if(newtemp!=nullptr && newtemp->next==nullptr){//IF SPOT OCCUPIED & NEXT UNOCCUPIED
-					newtemp->next=oldtemp;//place
-					oldtemp=oldtemp->next;//iterate
-					newtemp->next->next=nullptr;//patch
-				}
-				else if(newtemp!=nullptr && newtemp->next!=nullptr){//IF SPOT OCCUPIED & NEXT IS OCCUPIED
-					newtemp=newtemp->next;//iterate
-					chainlen++;
-				}
-				if(chainlen==4){cout<<"Another Chain Overflow Has Ocurred, Rehash will loop until resolved."<<endl;rhtf=true;}//MOVE/REHASH, THEN INCREMENT TEMP
+bool rehash(student** &table, int &tablesize){//automatically (when called) re-spreads the items into a new hash table
+	bool rhtf=false;
+	int newsize = 2*tablesize;
+	student** newtable = new student*[newsize];//assigns 'global' table variable to new table of double size
+	for(int i=0;i<newsize;i++){newtable[i]=nullptr;}//fills table with empty head pointers
+
+	for(int i=0;i<tablesize;i++){//FOR EVERY OLD STUDENT
+		student* curr = table[i];//current pointer
+		while(curr!=nullptr){//finding loop
+			student* next = curr->next;//saves next pointer
+			curr->next=nullptr;//clears direction of plucked student
+
+			int index = 0;
+			for(char c : curr->name){index += (static_cast<int>(c) + 47) * 109;}
+			index %= newsize;
+			if(newtable[index]==nullptr){newtable[index]=curr;}
+			else{
+				student* temp = newtable[index];
+				int chainlen=1;
+				while(temp->next!=nullptr){temp=temp->next;chainlen++;}//iterate to end of chain
+				temp->next=curr;
+				if(chainlen>=4){rhtf=true;}
+			}
+			curr=next;
 		}
 	}
+	delete[] table;
 	table = newtable;//Finally reassign the 'global' table to the new table
-	//DELETE THE OLD TABLE SOMEHOW
+	tablesize=newsize;
 	return rhtf;//RETURNS WHETHER OR NOT ANOTHER REHASH IS NEEDED
 }
-void add(int &idcount,student** &table){//adds one student, INCLUDES HASH FUNCTION, HASH FROM STUDENT ID
+
+void add(int &idcount,student** &table, int &tablesize){//adds one student, INCLUDES HASH FUNCTION, HASH FROM STUDENT ID
 	string _name;
-	double _gpa;
+	double _gpad;
+	string _gpa;
 	cout<<"You are adding a student"<<endl;
 	cout<<"Name(First Last): ";
 	getline(cin,_name); cout<<endl;
 	cout<<"GPA: ";
 	getline(cin,_gpa); cout<<endl;
-	double _gpaint=stod(_gpa);
-	student* s = new student(_name,_id,_gpaint, nullptr, nullptr);//change linked list directions
-	student* h=s;
-	int index;
-	for(char i ; _name){index += (static_cast<int>(i) + 47) * 109;}//HASH TIME UNDO BY /109, -47
-	index %= (sizeof(table)/sizeof(table[0]));
+	_gpad=stod(_gpa);
+	student* s = new student(_name,idcount++,_gpad, nullptr, nullptr);//change linked list directions
+	int index = 0;
+	for(char i : _name){index += (static_cast<int>(i) + 47) * 109;}//HASH TIME UNDO BY /109, -47
+	index %= tablesize;
+	int chainlen = 1;
+	if(table[index]==nullptr){table[index]=s;}
 	else{
-		int chainlen = 1;
 		student* temp = table[index];
-		while(searching){
-			if(temp==nullptr){temp = s; chainlen++;}
-			else if(temp!=nullptr && temp->next==nullptr){temp->next = s;s->prev=temp;chainlen++;}//IF NO ENTRIES, ADD
-			else if(temp!=nullptr && temp->next!=nullptr){temp=temp->next;chainlen++;}//increment if not end
-			if(chainlen==4){
-				bool rehashing;
+		while(temp->next!=nullptr){temp=temp->next;chainlen++;}//iterate to end of chain
+			temp->next=s;
+			if(chainlen>=4){
 				cout<<"Chain Overflow, rehashing..."<<endl;
-				while(rehashing){if(rehash(&table)==true){continue;}else if(rehash(&table)==false){cout<<"Rehashed"<<endl;rehashing=false;searching=false;}
-			}//CONDITIONAL TO REHASH
-		}
-	}//ITERATE TO LAST ELEMENT AND KEEP TRACK OF LENGTH TO DETERMINE REHASH
+				while(rehash(table,tablesize)){//rehashes until rehash returns false
+					cout<<"Chain Overflow; Rehashing..."<<endl;
+				}
+				cout<<"Rehashed"<<endl;
+			}
+	}
 }
-void gen(int &idcount,string &firstnames,string &lastnames,student** &table){//generates x number of students
+
+void gen(int &idcount,vector<string> &firstnames,vector<string> &lastnames,student** &table,int &tablesize){//generates x number of students
 	string numstr;
 	int num;
 	cout<<"You are generating any number of students randomly"<<endl;
@@ -104,33 +107,46 @@ void gen(int &idcount,string &firstnames,string &lastnames,student** &table){//g
 		mt19937 gen(rd());//random generator
 		uniform_real_distribution<double> gpadist(0.00,4.5);
 		_gpa = gpadist(gen);//assign gpa
-		uniform_int_distribution<int> namedist(0, v.size()-1);//generates random index based on vector size (40 items in this case)
+		uniform_int_distribution<int> namedist(0, firstnames.size()-1);//generates random index based on vector size (40 items in this case)
 		string randomfirst = firstnames[namedist(gen)];
 		string randomlast = lastnames[namedist(gen)];
 		_name = randomfirst+" "+randomlast;//assign name
-		add(_name,_id,_gpa);//adds the randomly generated student REPLACE
+		int index;
+		for(char i : _name){index += (static_cast<int>(i) + 47) * 109;}//HASH TIME UNDO BY /109, -47
+		index %= tablesize;
+		int chainlen = 1;
+		student* temp = table[index];
+		while(searching){
+			if(temp==nullptr){temp = s; chainlen++;}
+			else if(temp!=nullptr && temp->next==nullptr){temp->next = s;chainlen++;}//IF NO ENTRIES, ADD
+			else if(temp!=nullptr && temp->next!=nullptr){temp=temp->next;chainlen++;}//increment if not end
+			if(chainlen==4){
+				bool rehashing;
+				cout<<"Chain Overflow, rehashing..."<<endl;
+				while(rehashing){if(rehash(&table)==true){continue;}else if(rehash(&table)==false){cout<<"Rehashed"<<endl;rehashing=false;searching=false;}
+			}//CONDITIONAL TO REHASH
+		}
 	}
 }
-int main{
+int main(){
 	string line;//variable initializations, first and lastname vectors, run contidion, id count, table.
 	ifstream file1("firstnames");vector<string> firstnames;//file1 and corresponding vector
 	while(getline(file1, line)){firstnames.push_back(line);}
 	ifstream file2("lastnames");vector<string> lastnames;//file2 and corresponding vector
 	while(getline(file2, line)){lastnames.push_back(line);}
 	string input;
-	bool listing = true;
+	bool listing=true;
 	int idcount = 111111;
-	//int primecount = 0;
-	//int primes[] = {101,211,431,863,1733,3467};
-	student** table = new student*[101];//actual hash table
-	for(int i=0;i<100;i++){student* h=nullptr;table[i]=h;}//fills table with empty head pointers
+	tablesize = 101;
+	student** table = new student*[tablesize];//actual hash table
+	for(int i=0;i<101;i++){student* h=nullptr;table[i]=h;}//fills table with empty head pointers
 	while(listing){//main loop
 		cout<<"Enter a command (GENERATE, ADD, DELETE, PRINT, QUIT): "<<endl;
 		getline(cin, input);
-		if(input=="ADD"){add(&idcount,&table);}
-		else if(input=="GENERATE"){gen(&idcount,&firstnames,&lastnames);}
-		else if(input=="DELETE"){dl(&table);}
-		else if(inpnut=="PRINT"){print(&table);}
+		if(input=="ADD"){add(idcount,table,tablesize);}
+		else if(input=="GENERATE"){gen(idcount,firstnames,lastnames,tablesize);}
+		else if(input=="DELETE"){dl(table,tablesize);}
+		else if(input=="PRINT"){print(table,tablesize);}
 		else if(input=="QUIT"){listing = false;}
 		else{cout<<"Invalid input, please try again."<<endl;}
 	}
