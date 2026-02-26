f1c630a023bb85aaa1f1a9ddd4388a1588874d68
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2026-02-26 13:23:52 -0800

    DT2/26.2

--- heap/main.cpp
+++ heap/main.cpp
@@ -5,35 +5,55 @@ using namespace std;
 void print(){
 
 }
-void add(int &tree , bool fileorinput){
-	if(fileorinput==true){
-		
-	}
-	else if(fileorinput==false){
-		
-	}
-}
-void dlroot(int &tree){
+void add(int &tree, string filename){//add function for file input
 	
 }
+void add(int &tree, int number){//add function for manual input
+	
+}
+int rmroot(int &tree){
+	return 0;
+}
+int rmall(int &tree){
+	return 0;
+}
 void main(){
+	//'global' variables
 	int tree[100] = {};
-
+	for(int i=0;i<100;i++;){tree[i]=0;}
 	string input = "";
 	bool working = true;
-	while(working){
-		cout<<"Enter a command (add, delete root, print): ";
+	while(working){//main input loop
+		cout<<"Enter a command (add, remove root, remove all, print): ";
 		getline(cin, input);
+		cout<<endl;
 		if(input=="add"){
 			string boolin == "";
 			cout<<"Add by input or file?: ";
 			getline(cin, boolin);
-			if(boolin==file){add(tree,true);}
-			else if(boolin==input){add(tree,false);}
+			cout<<endl;
+			if(boolin==file){
+				string filename = "";
+				cout<<"Enter filename: ";
+				getline(cin, filename);
+				add(tree,filename);
+			}
+			else if(boolin==input){
+				string numstr = "";
+				cou<<"Enter a number to add (1-1000): ";
+				getline(cin, number);
+				cout<<endl;
+				int number = stoi(numstr);
+				if(1000<number<1){cout<<"Out of range, please retry."<<endl;}
+				else{add(tree,number);}
+			}
 			else{cout<<"Invalid input, please retry."<<endl;}
 		}
-		else if(input=="delete root"){
-			dlroot(tree);
+		else if(input=="remove root"){
+			rmroot(tree);
+		}
+		else if(input=="remove all"){
+			rmall(tree);
 		}
 		else if(input=="print"){
 			print(tree);
