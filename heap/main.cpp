8cb9c796421e47dd376be33b65d62ef17f8bad10
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2026-03-05 11:49:20 -0800

    DT3/5

--- heap/main.cpp
+++ heap/main.cpp
@@ -2,46 +2,52 @@
 #include<fstream>
 #include<string>
 using namespace std;
-void print(){
-
+void print(int tree[]){
+	cout<<tree<<endl;
 }
-void balancedown(int &tree, int size, int i){
+/*
+void balancedown(int tree[], int &size, int i){
 	int largest = i;//current largest index
 	int ch1 = 2*i +1;//child root calculation
 	int ch2 = 2*i +2;
+	return;
 }
-void balanceup(int &tree, int size, int current){
-	//current represents current index when added
-	int parent = 0;//All LEFT children have ODD indices, and all EVEN have EVEN indices
-	if(index%2=0){parent = (index-2)/2;}//index even
-	else{parent = (index-1)/2;}//else index odd
+*/
+void balanceup(int tree[], int &size, int currentindex){//SEGFAULT BEGINS HERE
+	if(currentindex=0){cout<<"Ordered"<<endl;return;}
+	int parentindex = 0;
+	if(currentindex % 2 == 0){parentindex = (currentindex-2)/2;}//index even
+	else{parentindex = (currentindex-1)/2;}//else index odd
 	int temp = 0;
-	if(tree[parent]<tree[index]){temp = tree[parent];tree[parent]=tree[index];tree[index]=temp;}
+	if(tree[parentindex]<tree[currentindex]){temp = tree[parentindex];tree[parentindex]=tree[currentindex];tree[currentindex]=temp;}
+	else{return;}
+	balanceup(tree, size, parentindex);
 }
-void add(int &tree, string filename){//add function for file input
-	
+void add(int tree[], string filename, int &size){//add function for file input
+	return;
 }
-void add(int &tree, int number, int &size){//add function for manual input
+void add(int tree[], int number, int &size){//add function for manual input
 	int index = 0;
-	while(tree[index]!=0){i++;}
+	if(tree[0]!=0){while(tree[index]!=0){index++;}}
 	tree[index]=number;
-	balanceup(tree,size,index)//INDEX PASSED AS CURRENT SLOT TO MOVE UP FROM
+	cout<<"Number added, re-ordering..."<<endl;
+	balanceup(tree,size,index);//INDEX PASSED AS CURRENT SLOT TO MOVE UP FROM
 }
-int rmroot(int &tree, int &size){
+int rmroot(int tree[], int &size){
 	tree[0]=tree[size-1];
 	tree[size-1]=0;
 	size-=1;
-	balancedown(tree,size,0);//REBALANCES NODES
+	//balancedown(tree,size,0);//REBALANCES NODES
 	return 0;
 }
-int rmall(int &tree){
+int rmall(int tree[]){
 	return 0;
 }
-void main(){
+int main(){
 	//'global' variables
 	int tree[100] = {};
-	size = 100;//initial max size
-	for(int i=0;i<100;i++;){tree[i]=0;}
+	int size = 100;//initial max size
+	for(int i=0;i<100;i++){tree[i]=0;}
 	string input = "";
 	bool working = true;
 	while(working){//main input loop
@@ -49,29 +55,29 @@ void main(){
 		getline(cin, input);
 		cout<<endl;
 		if(input=="add"){
-			string boolin == "";
+			string addtype = "";
 			cout<<"Add by input or file?: ";
-			getline(cin, boolin);
+			getline(cin, addtype);
 			cout<<endl;
-			if(boolin==file){
+			if(addtype=="file"){
 				string filename = "";
 				cout<<"Enter filename: ";
 				getline(cin, filename);
 				add(tree,filename,size);
 			}
-			else if(boolin==input){
+			else if(addtype=="input"){
 				string numstr = "";
-				cou<<"Enter a number to add (1-1000): ";
-				getline(cin, number);
+				cout<<"Enter a number to add (1-1000): ";
+				getline(cin, numstr);
 				cout<<endl;
 				int number = stoi(numstr);
-				if(1000<number<1){cout<<"Out of range, please retry."<<endl;}
+				if(number<1 || number>1000){cout<<"Out of range, please retry."<<endl;}
 				else{add(tree,number,size);}
 			}
 			else{cout<<"Invalid input, please retry."<<endl;}
 		}
 		else if(input=="remove root"){
-			rmroot(tree);
+			rmroot(tree, size);
 		}
 		else if(input=="remove all"){
 			rmall(tree);
@@ -82,4 +88,5 @@ void main(){
 		else if(input=="quit"){working=false;}
 		else{cout<<"Invalid input, please try again."<<endl;}
 	}
+	return 0;
 }
