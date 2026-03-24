0ee2da5ec77e85fea5d27ee1365fcf650e0b9f98
EmilioV07 <493914@bsd48.org>
2026-03-23 21:09:07 -0700

    LT3/23.2

--- sy/sy.cpp
+++ sy/sy.cpp
@@ -1,2 +1,26 @@
 #include<iostream>
 #include<string>
+
+int main(){
+	string input;//arithmetic input
+	char outOption;//output/notation preference
+	bool running=true;
+	while(running){//main input loop
+		switch (outOption){
+		case '1':
+			std::cout<<"1"<<std::endl;
+			break;
+		case '2':
+			std::cout<<"2"<<std::endl;
+			break;
+		case '3':
+			std::cout<<"3"<<std::endl;
+			break;
+		case 'q':
+			std::cout<<Quitting...<<endl;
+			return 0;//exit program ADD DELETION/CLEANUP IF NEEDED
+		default:
+			std::cout<<"Invalid option, try again"<<std::endl;
+	}
+	}
+}
