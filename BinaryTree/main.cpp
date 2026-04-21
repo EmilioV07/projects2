5da1813166ee83fc34dd37a2036f9c0f46e28b19
EmilioV07 <493914@bsd48.org>
2026-04-21 13:10:11 -0700

    LT4/21 Directory initialization, starting code

---
+++ BinaryTree/main.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+
+struct node{
+	node* prev;
+	node* left;
+	node* right;
+	char data;
+	node(char data, node* prev, node* left, node* right);
+};
+
+struct tree{
+	node* root;
+	tree(node* _root) : root(_root) {}
+
+	void print(node* root, int depth = 0){//print tree sideways with tab spacing
+		if(root==nullptr){return;}//return if tree is empty
+		print(root->right, depth+1);//otherwise (else), recursively increase (find) depth
+		for(int i=0; i<depth; i++){std::cout<<"    ";}//depth spacing
+		std::cout<<root->data<<'\n';//depth-based tab spacing
+		print(root->left, depth + 1);
+		}
+	}
+
+	void add(node* _inputNode){
+		
+	}
+
+	//wrappers
+	void print(){print(root);}
+
+	~tree(){//clears tree
+		return;
+	}
+};
+
+int main(){
+	bool running = true;
+	tree* tree = new tree(nullptr);//initialize tree
+	
+	while(running){
+		char input = ' ';
+		//input cycle
+		std::cout<<"Input (1 ADD, 2 DELETE ROOT, 3, Q QUIT): ";
+		std::cin>>input; std::cout<<std::endl;//grab instruction
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');//set cin \n ignore for later getline(s)
+		
+		//input decision
+        switch (input){
+        case '1':
+            std::cout<<std::endl;
+            break;
+        case '2':
+            std::cout<<std::endl;
+            break;
+        case '3':
+            std::cout<<std::endl;
+            break;
+        case 'Q':{
+                //exit program ADD DELETION/CLEANUP IF root!=nullptr
+                return 0;
+            }
+        default:
+            std::cout<<std::endl<<"Invalid option, try again"<<st>
+        }
+	}
+}
