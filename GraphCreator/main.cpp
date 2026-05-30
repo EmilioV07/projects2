2a6f65dbde20da4fd7d06645c04d51508cac946b
EmilioV07 <493914@bsd48.org>
2026-05-29 23:54:01 -0700

    as final as final gets I guess

--- GraphCreator/main.cpp
+++ GraphCreator/main.cpp
@@ -13,10 +13,10 @@ struct graph{
 	};
 	*/
 
-	int vertCount = 0;//keep track of vertices
+	int vertCount = 0;//keep track of vertices, so far redundant, keeping just in case
 
-	vector<std::string> labels;//stores label for each vertex
-	vector<vector<pair<int,int>>> adj;//adjacency list. vector of vectors of int pairs used to store edges and weight of said edges (edge-neighbor, weight)
+	std::vector<std::string> labels;//stores label for each vertex
+	std::vector<std::vector<std::pair<int,int>>> adj;//adjacency list. vector of vectors of int pairs used to store edges and weight of said edges (edge-neighbor, weight)
 
 	void addVert(){//add a vertex to the graph
 		std::string label;
@@ -31,48 +31,57 @@ struct graph{
 		std::string startstr;
 		std::string endstr;
 		std::string weightstr;
+		int weight;
 		int start;
 		int end;
 
-		std::cout<<"Enter a vertex index to add an edge to: ";
+		std::cout<<"Enter a vertex index to add an edge to: ";//grab start vertex
 		std::getline(std::cin, startstr);
-		start= = stoi(startstr);
+		start = stoi(startstr);
 		std::cout<<std::endl;
+		if(start<0 || start >= vertCount){std::cout<<"Invalid input, please try again."<<std::endl; return;}
 
-		std::cout<<"Enter a destination vertex: ";
+		std::cout<<"Enter a destination vertex: ";//grab destination vertex
 		std::getline(std::cin, endstr);
 		end = stoi(endstr);
 		std::cout<<std::endl;
+		if(end<0 || end >= vertCount){std::cout<<"Invalid input, please try again."<<std::endl; return;}
 
-		std::cout<<"Enter edge weight: ";
+		std::cout<<"Enter edge weight: ";//grab desired edge weight
 		std::getline(std::cin, weightstr);
+		weight = stoi(weightstr);
 		std::cout<<std::endl;
 
-		adj.push_back();
+		adj[start].push_back({end, weight});//push new edge to adjacency list
 	}
 };
 
 int main(){
 	bool running = true;
-	graph* graph = new graph();
+	graph* g = new graph();
+	while(running){
+		char input = ' ';
+		std::cout<<"Input (1 ADD VERTEX, 2 ADD EDGE, Q QUIT): ";
+	    std::cin>>input; std::cout<<std::endl;//grab instruction
+	    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');//set cin \n ignore for later getlines
 
-	char input = ' ';
-	std::cout<<"Input (1 ADD VERTEX, 2 DELETE VERTEX, Q QUIT): ";
-    std::cin>>input; std::cout<<std::endl;//grab instruction
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');//set cin \n ignore for later getlines
-
-	switch(input){
-		case '1':
-		std::cout<<std::endl;
-		graph->addVert();//add vertex case
-		break;
-		case '2':
-		std::cout<<std::endl;
-		graph->addEdge();//add edge case
-		break;
-		case 'Q':
-		delete graph;//how conventient
-		running = false;
-		break;
+		switch(input){
+			case '1':
+			std::cout<<std::endl;
+			g->addVert();//add vertex case
+			break;
+			case '2':
+			std::cout<<std::endl;
+			g->addEdge();//add edge case
+			break;
+			case 'Q':
+			delete g;//how conventient
+			running = false;
+			break;
+			default:
+			std::cout<<std::endl;
+			std::cout<<"Invalid input, please try again."<<std::endl;
+			break;
+		}
 	}
 }
